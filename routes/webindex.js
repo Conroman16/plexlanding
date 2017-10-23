778b7b9718dd108d89f1387db6276bcf7f248207
Connor Kennedy <cjtkennedy@gmail.com>
2017-10-23 02:12:02 -0500

    Serve up links via API

--- routes/webindex.js
+++ routes/webindex.js
@@ -5,5 +5,33 @@ module.exports = () => {
 
     router.get('/', (req, res) => res.render('index/index'));
 
+    router.get('/links', (req, res) => {
+    	res.json({
+			links: [
+				{
+					href: "https://plexpy.cjtkennedy.com",
+					name: "PlexPy",
+					order: 5
+				},
+				{
+					href: 'https://plexrequests.cjtkennedy.com',
+					name: 'Plex Requests',
+					order: 2
+				},
+				// {
+				// 	href: 'mailto:plexadmin@cjtkennedy.com',
+				// 	name: 'Email Administrator',
+				// 	order: 1
+				// },
+				{
+					href: "https://plexweb.cjtkennedy.com",
+					name: "Plex",
+					class: "button button--primary button--rounded",
+					order: 10
+				}
+			]
+		});
+    });
+
     return router;
 };
